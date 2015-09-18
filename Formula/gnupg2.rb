2357e9e7286c0dfce56223d3b8a08c2ec28ed665
Krzysztof Kotowicz <koto@google.com>
2015-09-18 16:01:35 +0200

    Upgrade to 2.1.8

--- Formula/gnupg2.rb
+++ Formula/gnupg2.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Gnupg2 < Formula
   homepage "https://www.gnupg.org/"
-  url "ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.5.tar.bz2"
-  mirror "ftp://ftp.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.5.tar.bz2"
-  mirror "ftp://mirror.tje.me.uk/pub/mirrors/ftp.gnupg.org/gnupg/gnupg-2.1.5.tar.bz2"
-  sha1 "02bbe32e1ef3b06b3ae3c60c955fb767a1aa0f2f"
-  revision 3
+  url "ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.8.tar.bz2"
+  mirror "ftp://ftp.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.8.tar.bz2"
+  mirror "ftp://mirror.tje.me.uk/pub/mirrors/ftp.gnupg.org/gnupg/gnupg-2.1.8.tar.bz2"
+  sha1 "61f5bc656dd7fddd4ab67b720d47ef0651bfb727"
+  revision 4
 
   option "8192", "Build with support for private keys of up to 8192 bits"
 
